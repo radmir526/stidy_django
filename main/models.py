fc92e71d3b460d13d425e5129bf56b29715cf7aa
radmir <rrafizovic@gmail.com>
2026-01-30 16:39:40 +0500

    сделали шаблоны страниц, views, настроили urls, исправление ошибок в коде

--- main/models.py
+++ main/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Тут мы прописываем все необходимое для нашей базы данных
 
@@ -15,12 +17,15 @@ class Category(models.Model): # это класс для категорий
     def __str__(self): # этот метод определяет то, как у нас будет отображаться тот или иной объект в админке
         return self.name
     
+    def get_absolute_url(self):
+        return reverse('main:product_list_by_category', args=[self.slug])
+
 
 class Product(models.Model): # Прописываем модель нашего продукта
     category = models.ForeignKey(Category, related_name='products', # в эту переменную мы засунули класс Category
                                  on_delete=models.CASCADE)
     name = models.CharField(max_length=100, db_index=True) # Устанавливаем наименование продукта
-    slug = models.SlugField(max_length=100, unique=100) # Устанавливаем ссылки для продуктов
+    slug = models.SlugField(max_length=100, unique=True) # Устанавливаем ссылки для продуктов
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True) # Добавляем поле с фотографиями, которые будут сохранятся в папки относительно года, месяца и дня загрузки этих фотографий, так же добавили возможность не загружать фото вовсе
     description = models.TextField(blank=True) # устанавливаем описание товара
     price = models.DecimalField(max_digits=10, decimal_places=2)  # устанавливаем прайс с числом с плавающей точкой
@@ -33,4 +38,9 @@ class Product(models.Model): # Прописываем модель нашего 
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        # Возвращает URL для страницы с товарами этой категории.
+        # Используется в шаблонах для ссылок без хардкода путей. Мы не пишем сами ссылки, а они автоматически подставляются при нажатии на какой либо продукт
+        return reverse("main:product_detail", args=[self.id, self.slug])
     
--- main/views.py
+++ main/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+# во views мы обрабатываем запросы пользователя и отправляем ему соответствующие ответы
+# так же добавляем представления для шаблонов
 
-# Create your views here.
+
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
+
+
+
+# Используем функции, потому что сайт не большой, а если было бы большое количество инфы, то использовали бы классы
+def product_list(request, category_slug=None):
+    categories = Category.objects.all() # Это параметр который будет выводить все наши категории
+    products = Product.objects.filter(available=True)
+
+    category = None
+    if category_slug: # Если в запросе от пользователя есть параметр на фильтрацию, то
+        category = get_object_or_404(Category, slug=category_slug) # мы получаем с базы данных категории с помощью get_object_or_404
+        products = products.filter(category=category) # далее мы все продукты, которые у нас есть, фильтруем по выбранной категории
+
+    return render(request, 'main/product/list.html',  # возвращаем юзеру шаблон страницы и третим параметром передаем context, именно эти параметры позволяют нам обращаться к категориям или продуктам
+                  {'category': category,
+                   'categories': categories,
+                   'products': products})
+
+
+def product_detail(request, id, slug): # страница товара, более подробно 
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    related_products = Product.objects.filter(category=product.category, # так же предлагаем похожие продукты пользователю
+                                              available=True).exclude(id=product.id)[:4] # c помощью exclude мы исключаем id того продукта, который уже выбрал польльзователь
+    
+    return render(request, 'main/product/detail.html', {'product': product,
+                                                        'related_products': related_products})
